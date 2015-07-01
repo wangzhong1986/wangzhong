e791e0b35b72f8e95b492d7139a0d26a7f634edd
wangzhong <wangzhong@babydeMacBook-Pro.local>
2015-07-01 19:00:26 +0800

    工程初始化

---
+++ firstProjectOnGitHub/firstProjectOnGitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  firstProjectOnGitHub
+//
+//  Created by wangzhong on 15/7/1.
+//  Copyright (c) 2015年 wangzhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ firstProjectOnGitHub/firstProjectOnGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  firstProjectOnGitHub
+//
+//  Created by wangzhong on 15/7/1.
+//  Copyright (c) 2015年 wangzhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
